85590242d93966d2fc8cae49725797250f8e00ff
Your Name <karoliina.mietola@helsinki.fi>
2018-11-05 20:26:28 +0200

    virheenkorjausta

--- NhlStatistics1/src/test/java/StatisticsTest.java
+++ NhlStatistics1/src/test/java/StatisticsTest.java
@@ -5,7 +5,6 @@
  */
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
@@ -24,19 +23,18 @@ import static org.junit.Assert.*;
  * @author kape
  */
 public class StatisticsTest {
+    ArrayList<Player> players;
     
-    Reader readerStub=new Reader() {
-                public List<Player> getPlayers() {
-            ArrayList<Player> players = new ArrayList<Player>();
- 
-            players.add(new Player("Semenko", "EDM", 4, 12));
-            players.add(new Player("Lemieux", "PIT", 45, 54));
-            players.add(new Player("Kurri",   "EDM", 37, 53));
-            players.add(new Player("Yzerman", "DET", 42, 56));
-            players.add(new Player("Gretzky", "EDM", 35, 89));
- 
-            return players;
-        }
+    Reader readerStub=() -> {
+        players = new ArrayList<>();
+        
+        players.add(new Player("Semenko", "EDM", 4, 12));
+        players.add(new Player("Lemieux", "PIT", 45, 54));
+        players.add(new Player("Kurri",   "EDM", 37, 53));
+        players.add(new Player("Yzerman", "DET", 42, 56));
+        players.add(new Player("Gretzky", "EDM", 35, 89));
+        
+        return players;
     };
     
     Statistics stats;
